28983454c88979cf09f637c9d5e02972284842ab
Cristian1312 <cristianperincio1312@gmail.com>
2025-11-15 18:15:10 -0500

    Project start

---
+++ src/app/pages/custom-page/custom-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-page',
+  imports: [],
+  templateUrl: './custom-page.html'
+})
+export default class CustomPage {
+
+}
