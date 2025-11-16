13f3e050af0b39cd32140608ab82f8df346003fd
Cristian1312 <cristianperincio1312@gmail.com>
2025-11-16 17:03:54 -0500

    Decimal, Currency and Percent pipes

--- src/app/pages/numbers-page/numbers-page.ts
+++ src/app/pages/numbers-page/numbers-page.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.html'
 })
 export default class NumbersPage {
+
+  totalSales = signal(50_415_687.5629);
+
+  percent = signal(0.4876);
 
 }
