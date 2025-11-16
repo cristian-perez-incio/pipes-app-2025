0daa107baa2236eca175f85849d460b212b40ee7
Cristian1312 <cristianperincio1312@gmail.com>
2025-11-16 17:37:51 -0500

    Switch languages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
 
 import { routes } from './app.routes';
+
+import esPeLocal from '@angular/common/locales/es-PE';
+
+registerLocaleData(esPeLocal, 'es-PE');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-PE'
+    }
   ]
 };
