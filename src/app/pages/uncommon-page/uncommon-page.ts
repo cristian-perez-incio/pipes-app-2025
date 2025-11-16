4b3d1835296aebb689652a9449d1ddeba1e0f3a9
Cristian1312 <cristianperincio1312@gmail.com>
2025-11-16 18:42:27 -0500

    I18nSelect Pipe

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
 import { Card } from "../../components/card/card";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Alberto',
+  gender: 'male',
+  age: 35,
+  address: 'Lima, Peru'
+}
+
+const client2 = {
+  name: 'Teresa',
+  gender: 'female',
+  age: 38,
+  address: 'Tarapoto, Peru'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card],
+  imports: [Card, I18nSelectPipe],
   templateUrl: './uncommon-page.html'
 })
 export default class UncommonPage {
+
+  // i18nSelect Pipe
+  client = signal(client1);
+
+  salutationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+  switchClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
 
 }
