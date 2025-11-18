64d7d32460c639c84b477889a9781ca097921bbb
Cristian1312 <cristianperincio1312@gmail.com>
2025-11-18 17:45:58 -0500

    HeroTextColor and HeroCreator pipes

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -4,10 +4,16 @@ import { heroes } from '../../data/heroes.data';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
+  imports: [
+    ToggleCasePipe, CanFlyPipe, HeroColorPipe,
+    HeroTextColorPipe, HeroCreatorPipe, TitleCasePipe
+  ],
   templateUrl: './custom-page.html'
 })
 export default class CustomPage {
